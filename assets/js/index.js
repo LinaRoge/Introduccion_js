6c46d54f2e65cc2a37354fa6222ed2dfdaf0e80b
Roger <rogerlinarez@hotmail.com>
2024-03-01 02:09:58 -0800

    Se agrega desafio

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+precio = 400000;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+const cantidad = document.querySelector(".cantidad");
+cantidad.innerHTML;
+
+const valor = document.querySelector(".valor");
+valor.innerHTML;
